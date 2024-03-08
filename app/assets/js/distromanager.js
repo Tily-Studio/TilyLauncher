dedbbd801e20d03b280a0447fed4813869cb4ffd
Aness6040 <aness2805@gmail.com>
2024-03-08 15:46:55 +0100

    added ffv support

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,9 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://ffv-source-mrcm.vercel.app/source/distribution.json'
+
+//exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
